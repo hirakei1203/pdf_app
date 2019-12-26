b45a9d2dc9fde7b710e07042e5a62c88acf145d3
EC2 Default User <ec2-user@ip-172-31-39-82.ap-northeast-1.compute.internal>
2019-12-26 02:40:20 +0000

    setting.py

--- tutorial/tutorial/settings.py
+++ tutorial/tutorial/settings.py
@@ -121,3 +121,6 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.0/howto/static-files/
 
 STATIC_URL = '/static/'
+LOGIN_URL = '/login'
+LOGIN_RECENT_URL = 'pdfmr/top'
+LOGOUT_REDIRECT_URL = '/login'
