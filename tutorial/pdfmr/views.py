0d47f490b192cc32aac907ad80bcc54c74df3976
EC2 Default User <ec2-user@ip-172-31-39-82.ap-northeast-1.compute.internal>
2019-12-31 05:29:26 +0000

    delete3

--- tutorial/pdfmr/views.py
+++ tutorial/pdfmr/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 
 from django.views import generic
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth.decorators import login_required
 from django.conf import settings
 from .forms import UploadForm
 from .utils import create_excel
@@ -52,3 +53,25 @@ class ListView(LoginRequiredMixin, generic.TemplateView):
             'login_user_name' : login_user_name,
         }
         return context
+        
+@login_required
+def dell_file(request):
+    checks_value = request.POST.getlist('checks')
+    login_user_name = request.user.username
+    
+    if checks_value:
+        for file in checks_value:
+            path = os.path.join(settings.MEDIA_ROOT, "excel", login_user_name, file)
+            default_storage.delete(path)
+        return render(request, 'pdfmr/delete.html', {'checks_value': checks_value})
+    else:
+        login_user_name = request.user.username
+        file_list = default_storage.listdir(os.path.join(settings.MEDIA_ROOT, "excel", login_user_name))[1]
+        warning_message ="削除対象ファイルが選択されていません"
+        
+        context = {
+            'file_list': file_list,
+            'login_user_name':login_user_name,
+            'warning_message':warning_message,
+        }
+        return render(request, 'pdfmr/list.html', context)
